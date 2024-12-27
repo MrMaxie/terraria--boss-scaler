3d0a764904abb6167d6903e5f6cdfe13c2c9db4c
MrMaxie <mxmsyuu@gmail.com>
2024-12-27 16:33:40 +0100

    refactor: Update BossScalerConfig to validate and constrain input values

--- BossScalerConfig.cs
+++ BossScalerConfig.cs
@@ -8,12 +8,43 @@ namespace BossScaler
 
         [LabelKey("$Mods.BossScaler.Config.BossDamageMultiplier.Label")]
         [TooltipKey("$Mods.BossScaler.Config.BossDamageMultiplier.Tooltip")]
-        [Range(0.1f, 20f)]
-        public float BossDamageMultiplier { get; set; } = 1f;
+        [Range(0.1f, 10f)]
+        public float BossDamageMultiplier { get; set; }
 
         [LabelKey("$Mods.BossScaler.Config.BossHPMultiplier.Label")]
         [TooltipKey("$Mods.BossScaler.Config.BossHPMultiplier.Tooltip")]
         [Range(0.1f, 20f)]
-        public float BossHPMultiplier { get; set; } = 1f;
+        public float BossHPMultiplier { get; set; }
+
+        private static float ToRange(float value, float min, float max, float? def)
+        {
+            if (value < min)
+            {
+                return def ?? min;
+            }
+            if (value > max)
+            {
+                return def ?? max;
+            }
+            return value;
+        }
+
+        private void EnsureValidValues()
+        {
+            BossDamageMultiplier = ToRange(BossDamageMultiplier, 0.1f, 10f, 1f);
+            BossHPMultiplier = ToRange(BossHPMultiplier, 0.1f, 20f, 1f);
+        }
+
+        public override void OnChanged()
+        {
+            EnsureValidValues();
+        }
+
+        public override void OnLoaded()
+        {
+            EnsureValidValues();
+        }
+
+        public override bool NeedsReload(ModConfig pendingConfig) => false;
     }
 }
